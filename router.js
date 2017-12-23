a69dfd7b43c46ebe128f05e12e48bef573addeff
ChenJing <502269499@qq.com>
2017-12-23 13:09:36 +0800

    允许跨域请求

--- router.js
+++ router.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
 
+//请求出口
+router.use(function(req, res, next) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  
+});
+
 //路由GET请求
 router.post('/api/ajax', (req, res) => {
 	res.send({ msg: 'You sent the message:' + req.body.sendMsg });

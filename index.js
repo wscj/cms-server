5f14009a1e3a1b48b18df84aa5e5fcc604a3519f
ChenJing <502269499@qq.com>
2017-12-29 15:46:16 +0800

    update

--- index.js
+++ index.js
@@ -19,3 +19,22 @@ app.get('/', (req, res) => {
 app.listen(7878, () => {
 	console.log('listening on port 7878');
 });
+
+// 开启https服务
+// var fs = require('fs');
+// var http = require('http');
+// var https = require('https');
+// var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+// var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+
+// var credentials = {key: privateKey, cert: certificate};
+// var express = require('express');
+// var app = express();
+
+// // your express configuration here
+
+// var httpServer = http.createServer(app);
+// var httpsServer = https.createServer(credentials, app);
+
+// httpServer.listen(8080);
+// httpsServer.listen(8443);
